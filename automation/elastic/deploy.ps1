b58de864d954319f54068959326e0072e39dee1e
Carlo Wahlstedt <carlowahlstedt@gmail.com>
2020-08-26 15:14:26 -0400

    go to 5 seconds

--- automation/elastic/deploy.ps1
+++ automation/elastic/deploy.ps1
@@ -15,7 +15,7 @@ Write-Output ""
 Write-Output "👀 Showing the ECK operator pod"
 kubectl get pods -n $Namespace
 
-Start-Sleep -Seconds 1
+Start-Sleep -Seconds 5
 Write-Output ""
 Write-Output "😬 Waiting for the ECK operator to complete"
 kubectl wait --for=condition=Ready -n $Namespace pod/elastic-operator-0 --timeout=60s
