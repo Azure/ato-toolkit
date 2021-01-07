9cfa8ad16bdbe649b629ed9e8bc32aabdbedce0f
Shruti Priya <shpriya@microsoft.com>
2021-01-07 12:31:09 -0800

    updated deployment script

--- stig/linux/scripts/deploySTIG.sh
+++ stig/linux/scripts/deploySTIG.sh
@@ -1,7 +1,15 @@
 echo "Installing scap-security-guide..."
 yum -y install scap-security-guide
+
+#Check if the script to apply stig exists
+STIG_FILE=/usr/share/scap-security-guide/bash/$1
+if [ ! -f "$STIG_FILE" ]; then
+    echo "$STIG_FILE does not exist."
+    exit 1
+fi
+
 echo "Modifying rhel7-script-stig.sh to exlude WALinuxAgent from updates..."
-sed 's/yum -y update/yum -y update --exclude=WALinuxAgent/g' /usr/share/scap-security-guide/bash/rhel7-script-stig.sh > rhel7-script-stig-updated.sh
-echo "Running hel7-script-stig-updated.sh to deploy STIGs..."
-bash rhel7-script-stig-updated.sh
+sed 's/yum -y update/yum -y update --exclude=WALinuxAgent/g' $STIG_FILE > script-stig-updated.sh
+echo "Running script-stig-updated.sh to deploy STIGs..."
+bash script-stig-updated.sh
 echo "Deployment completed successfully."
