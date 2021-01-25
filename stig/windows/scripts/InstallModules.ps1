a3f269d007e7ef4cd9485c9d577472cfd7af30a8
unknown <erjenkin@microsoft.com>
2021-01-25 17:45:39 -0500

    updated

--- stig/windows/scripts/InstallModules.ps1
+++ stig/windows/scripts/InstallModules.ps1
@@ -14,3 +14,30 @@ foreach($requiredModule in $requiredModules) {
 
 # Increase the MaxEnvelope Size
 Set-Item -Path WSMan:\localhost\MaxEnvelopeSizekb -Value 8192
+
+# Copy checklist to local directory
+mkdir c:\Logs -Force -Confirm:$false
+
+#Get OS version and copy specific checklist file
+$osVersion = (Get-WmiObject Win32_OperatingSystem).Caption
+switch -Wildcard ($osVersion)
+{
+    "*2016*"
+    {
+        
+        $baseUri = "https://raw.githubusercontent.com/erjenkin/ato-toolkit/master/stig/windows/scripts/Checklists/Server2016.ckl"
+        Invoke-WebRequest -Uri $baseuri -OutFile "c:\Logs\Server2016.ckl"
+        break
+    }
+    "*2019*"
+    {
+        
+        $baseUri = "https://raw.githubusercontent.com/erjenkin/ato-toolkit/master/stig/windows/scripts/Checklists/Server2019.ckl"
+        Invoke-WebRequest -Uri $baseuri -OutFile "c:\Logs\Server2019.ckl"
+        break
+    }
+}
+
+#copy checklist generation script
+$baseUri = "https://raw.githubusercontent.com/erjenkin/ato-toolkit/master/stig/windows/scripts/Checklists/GenerateChecklist.ps1"
+Invoke-WebRequest -Uri $baseuri -OutFile "c:\Logs\GenerateChecklist.ps1"
