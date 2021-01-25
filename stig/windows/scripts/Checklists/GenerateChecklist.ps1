d34088ffabbf1eb59288f912d6cce41d10949f03
unknown <erjenkin@microsoft.com>
2021-01-25 17:25:39 -0500

    added checklist files

--- stig/windows/scripts/Checklists/GenerateChecklist.ps1
+++ stig/windows/scripts/Checklists/GenerateChecklist.ps1
@@ -1,3 +1,18 @@
+<#
+.SYNOPSIS
+    When “Run as administrator” this script will generate a checklist file via PowerSTIG for Windows Server 2016/2019 that can be viewed with DISA’s StigViewer 
+    (https://public.cyber.mil/stigs/srg-stig-tools/).
+    A checklist is included with this script as an example of the compliance status with manual checklist entries added.
+    Please confirm all security settings once deployed to your environment.
+.DESCRIPTION
+    This script is able to generate checklist files for Server 2019 and 2016, with applications installed on base images (Windows Defender, Internet Explorer, Windows Firewall,
+    and DotNet Framework 4)
+.NOTES
+    This script is included to assist with generating a checklist of a newly deployed VM. Modifications to the script may be required based on organization requirements
+.EXAMPLE
+    .\GenerateChecklist.ps1
+#>
+
 Import-Module PowerStig -verbose -force
 
 #Get OS version
@@ -22,7 +37,7 @@ $powerSTIGpath = (Get-Module -Name PowerSTIG).ModuleBase
 while((Get-DscLocalConfigurationManager).LCMState -notmatch "Idle")
 {
     start-sleep 5
-    write-host "Waiting 5 seconds for retry"
+    Write-Host "Waiting 5 seconds for retry"
 }
 
 $dscResults = Test-DscConfiguration -Detailed
@@ -33,7 +48,7 @@ $outputPath = "C:\$env:COMPUTERNAME.ckl"
 # Server STIGs
 $latestOsVersion = (Get-Stig -ListAvailable | Where-Object  {$_.TechnologyVersion -eq $OsVersion -and $_.TechnologyRole -eq "MS" }| Measure-Object -Maximum -Property Version).Maximum
 $serverOsSTIG = '{0}\StigData\Archive\Windows.Server.{1}\U_MS_Windows_Server_{1}_MS_STIG_V{2}R{3}_Manual-xccdf.xml' -f $powerSTIGpath,$OsVersion,$latestOsVersion.Major,$latestOsVersion.Minor
-$manServerSTIG  = "U_MS_Windows_Server_{0}_MS_STIG_V{1}R{2}_Manual-xccdf.xml" -f $OsVersion,$latestVersion.Major,$latestVersion.Minor
+$manServerSTIG  = "U_MS_Windows_Server_{0}_MS_STIG_V{1}R{2}_Manual-xccdf.xml" -f $OsVersion,$latestOsVersion.Major,$latestOsVersion.Minor
 
 # Windows Defender STIG
 $latestDefenderVersion = (Get-Stig -ListAvailable | Where-Object Technology -eq "WindowsDefender"| Measure-Object -Maximum -Property Version).Maximum
@@ -51,21 +66,20 @@ $manfirewallSTIG  = "U_Windows_Firewall_STIG_V{0}R{1}_Manual-xccdf.xml" -f $late
 
 # Array of STIGS to add to checklist
 $xccdfPath = @($serverOsSTIG,$defenderSTIG,$internetExplorerSTIG,$firewallSTIG)
-
 $status = "NotAFinding"
 $comments = "Not Applicable"
-$details = 'Not applicable for this VM as of deployment time - {0}' -f $(Get-Date)
+$details = 'Not applicable for this VM as of deployment time {0}' -f $(Get-Date)
 
 $manualRules = @(
     @{
         osVersion = "2019"
         stig = $manServerSTIG
-        id   =  @("V-205624","V-205657","V-205661","V-205664","V-205677","V-205699","V-205721","V-205727","V-205746","V-205844","V-205847","V-205848","V-205852","V-205853","V-205854")
+        id   =  @("V-205624","V-205657","V-205661","V-205664","V-205677","V-205699","V-205721","V-205727","V-205746","V-205844","V-205847","V-205848","V-205852","V-205853","V-205854","V-205855")
     },
     @{
         osVersion = "2016"
         stig = $manServerSTIG
-        id   =  @("V-224819","V-224820","V-224822","V-224823","V-224824","V-224825","V-224827","V-224836","V-224837","V-224841","V-224842","V-224843","V-224845","V-224848","V-224849","V-224860","V-224861","V-225007","V-224863")
+        id   =  @("V-224819","V-224820","V-224822","V-224823","V-224824","V-224825","V-224827","V-224836","V-224837","V-224841","V-224842","V-224843","V-224845","V-224848","V-224849","V-224860","V-224861","V-224863","V-225007")
     },
     @{
         osVersion = "2016|2019"
@@ -90,6 +104,7 @@ foreach($item in $manualRules)
     {
         foreach($rule in $item.id)
         {
+
             $writer.WriteStartElement("stigRuleData")
             $writer.WriteStartElement("STIG")
             $writer.WriteString($item.stig)
