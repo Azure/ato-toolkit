1571d12fed949a4810a68ca22bee132f46efa2e7
Eric Jenkins <eric.jenkins@microsoft.com>
2021-02-10 12:15:40 -0500

    update 2019

--- stig/windows/artifacts/InstallModules.ps1
+++ stig/windows/artifacts/InstallModules.ps1
@@ -1,16 +1,32 @@
-. "$PSScriptRoot\RequiredModules.ps1"
+Param(
+    [string] 
+    [Parameter(Mandatory = $false)]
+    $autoInstallDependencies = $false
+)
 
-# Added to support package provider download on Server 2016
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+if ($autoInstallDependencies -eq $true) {
+    . "$PSScriptRoot\RequiredModules.ps1"
 
-Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+    # Added to support package provider download on Server 2016
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-$requiredModules = Get-RequiredModules
+    Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 
-# Install the required modules
-foreach($requiredModule in $requiredModules) {
-    Install-Module -Name $requiredModule.ModuleName -RequiredVersion $requiredModule.ModuleVersion -Force
+    $requiredModules = Get-RequiredModules
+
+    # Install the required modules
+    foreach ($requiredModule in $requiredModules) {
+        Install-Module -Name $requiredModule.ModuleName -RequiredVersion $requiredModule.ModuleVersion -Force
+    }
 }
 
 # Increase the MaxEnvelope Size
 Set-Item -Path WSMan:\localhost\MaxEnvelopeSizekb -Value 8192
+
+# Set Password required for local accounts (STIG V-205700)
+Net user DefaultAccount /passwordreq:yes
+Net user Guest /passwordreq:yes
+
+# Set Local Admin account password expires True (V-205658)
+$localAdmin = Get-LocalUser | Where-Object Description -eq "Built-in account for administering the computer/domain"
+Set-LocalUser -name $localAdmin.Name -PasswordNeverExpires $false
